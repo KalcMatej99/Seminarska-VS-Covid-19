1e98e48e1f47a82750fdec0c9cb5b51302409649
KalcMatej99 <mk9693@student.uni-lj.si>
2020-07-26 21:37:28 +0200

    Finished first analisys

--- Programi/median_age_on_fatality_covid-19.r
+++ Programi/median_age_on_fatality_covid-19.r
@@ -1,19 +1,21 @@
 library(pracma)
-fileMedianAgePerEuCountry <- './../Podatki/Median_age_per_EU_country.csv'
-filePeakPerEuCountry <- './../Podatki/Peak_per_country.csv'
-dataMedianAge <- read.csv(fileMedianAgePerEuCountry, header=TRUE, sep=",")
-dataPeakPerContry<- read.csv(filePeakPerEuCountry, header=TRUE, sep=",")
+fileDB <- './../Podatki/db.csv'
+db<- read.csv(fileDB, header=TRUE, sep=",")
 
-dataMedianAge
-dataPeakPerContry
-
-numberOfEUCountries <- 27
-
-S <- dataMedianAge[, 2]
-F <- dataPeakPerContry[, 5]/dataPeakPerContry[, 4]
+S <- db$Median_age
+F <- db$Deaths_to_peak/db$Infected_to_peak
 
 plot(S,F,                                  
      main = "Vpliv mediane populacije na fatalnost Covid-19 v prvi polovici prvega vala",
      xlab = "Mediana starosti EU države",
      ylab = "Fatalnost EU države")
 abline(lm(F ~ S), col = "red")
+
+r1 <- cor(S, F,  method = "pearson")
+r2 <- cor(S, F,  method = "pearson", use = "complete.obs")
+s1 <- cor(S, F,  method = "spearman")
+s1 <- cor(S, F,  method = "spearman", use = "complete.obs")
+
+rsq = r1^2
+
+Fvalue = rsq/((2 - 1) * (1 - rsq) * (27 - 2))
