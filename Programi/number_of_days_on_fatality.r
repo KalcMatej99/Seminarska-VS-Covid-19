b151616fcbff0e5f5d96101d114d93db4c7e9fe7
KalcMatej99 <mk9693@student.uni-lj.si>
2020-07-30 17:44:41 +0200

    Finished first version

---
+++ Programi/number_of_days_on_fatality.r
@@ -0,0 +1,24 @@
+library(pracma)
+fileDB <- './../Podatki/db.csv'
+db<- read.csv(fileDB, header=TRUE, sep=",")
+
+dataFrameDates <- data.frame(date=db$Date_of_peak_of_deaths,tx_start=db$Date_of_first_death)
+dataFrameDates$date_diff <- as.Date(as.character(dataFrameDates$date), format="%Y-%m-%d")-
+  as.Date(as.character(dataFrameDates$tx_start), format="%Y-%m-%d")
+
+N <- as.numeric(dataFrameDates$date_diff)
+I <- (100 * db$Deaths_to_peak)/db$Infected_to_peak
+
+plot(N,I,                                  
+     main = "Vpliv števila dni do viška dnevnih mrtvih primerov na delež mrtvih v prvi polovici prvega vala",
+     xlab = "Število dni do viška dnevnih okuženih primerov EU države",
+     ylab = "Delež okuženih EU države")
+abline(lm(I ~ N), col = "red")
+
+r1 <- cor(N, I,  method = "pearson")
+s1 <- cor(N, I,  method = "spearman")
+k1 <- cor(N, I,  method = "kendall")
+
+rsq = r1^2
+
+Fvalue = rsq/((2 - 1) * (1 - rsq) * (27 - 2))
---
+++ Programi/proportion_infected.r
@@ -0,0 +1,34 @@
+library(pracma)
+require(graphics)
+fileDB <- '/home/matej/Documents/Seminarska-VS-Covid-19//Podatki/db.csv'
+db<- read.csv(fileDB, header=TRUE, sep=",")
+
+dataFrameDates <- data.frame(date=db$Date_of_peak_of_infected,tx_start=db$Date_of_first_infected)
+dataFrameDates$date_diff <- as.Date(as.character(dataFrameDates$date), format="%Y-%m-%d")-
+  as.Date(as.character(dataFrameDates$tx_start), format="%Y-%m-%d")
+
+inf <- (db$Infected_to_peak)/db$Population
+names <- db$Country_Name
+
+u = mean(inf)
+
+
+n <- 27
+
+t <- 1.96
+
+country <- c()
+leftMargin <- c()
+rightMargin <- c()
+
+for (ind in 1:27) {
+
+p <- inf[ind]
+margin <- t * sqrt((1 - p) * p / n)
+
+country <- append(country, as.character(names[ind]))
+leftMargin <- append(leftMargin, round(p * 100, digits = 3))
+rightMargin <- append(rightMargin, round((p + margin) * 100, digits = 3))
+}
+dataToPlot <- data.frame(country, leftMargin, rightMargin)
+write.csv(dataToPlot,'/home/matej/Documents/Seminarska-VS-Covid-19//Podatki/dbMargins.csv', row.names = FALSE)
