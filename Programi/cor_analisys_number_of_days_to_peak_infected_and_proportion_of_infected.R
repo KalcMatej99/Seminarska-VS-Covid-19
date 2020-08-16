2d9daea63699e0b6a23871b12d7e9d4083f1244e
KalcMatej99 <mk9693@student.uni-lj.si>
2020-08-16 17:35:26 +0200

    Added corrections

--- Programi/cor_analisys_number_of_days_to_peak_infected_and_proportion_of_infected.R
+++ Programi/cor_analisys_number_of_days_to_peak_infected_and_proportion_of_infected.R
@@ -19,3 +19,15 @@ s <- cor(N, P,  method = "spearman")
 
 rtest <- cor.test(N,P, method = "pearson")
 stest <- cor.test(N,P, method = "spearman")
+
+
+transf<-function(r){
+  0.5*log((1+r)/(1-r))}
+
+cor2.test<-function(r,rh0,n){
+  z<-(transf(r)-transf(rh0))*sqrt(n-3)/sqrt(1.06)
+  p<-1-pnorm(z)
+  c(paste("Test statistics=",round(z,3), ", ", "p-value=",round(p,3),sep=""))
+}
+
+cor2.test(r,0.3,27)
